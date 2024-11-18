6b5beea06dfbc5127bb1d0e0c0631d7f7632da8a
KIRAN NK <loki-NK@users.noreply.github.com>
2024-11-18 14:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if st.button("✨ Pick the Huddle Leader ✨"):
     st.markdown(
         f"""
         <div style="background-color:#ffeb3b;padding:15px;border-radius:10px;margin-top:20px;">
-            <h2 style="text-align:center;color:#000;">🎉 Next week's huddle will be handled by: 🎉 </h2>
+            <h2 style="text-align:center;color:#000;">🎉 Next week's huddle will be handled by: 🎉  </h2>
             <h1 style="text-align:center;color:#4CAF50;">🙌{random_name}</h1>
         </div>
         """,
